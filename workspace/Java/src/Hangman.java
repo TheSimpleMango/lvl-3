e6b96bbff3733db2a46adaa4953aa22c3dea739f
TheSimpleMango <mingda.he@gmail.com>
2015-09-13 14:31:08 -0700

    asdf

---
+++ workspace/Java/src/Hangman.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel; 
+import javax.swing.JPanel;
+
+
+public class Hangman {
+	static JFrame frame = new JFrame("Meme Hangman");
+	static JPanel panel = new JPanel();
+	static ArrayList<JLabel> labels = new ArrayList<JLabel>();
+	static ArrayList<String> words = new ArrayList<String>();
+	public static void main(String[] args) {
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		words.add("pepe");
+		words.add("feels guy");
+		words.add("miley what's good?");
+		words.add("JOOOOHHHN CENA");
+		words.add("");
+		for (int i = 0; i < 5; i++) {
+			labels.add(new JLabel("_"));
+		}
+		for (int i = 0; i < labels.size(); i++) {
+			panel.add(labels.get(i));
+		}
+		frame.pack();
+	}
+}
